1c7ac17066eebaa557f46fb3d9340a23bd858fd5
Estela23 <estela.monxardina@gmail.com>
2020-11-28 13:47:23 +0100

    updated non-optimal version of distance_metrics

--- distance_metric.py
+++ distance_metric.py
@@ -1,1 +1,40 @@
 # File where the three optional distances will be implemented
+import numpy as np
+
+
+def distances_metrics(train_matrix, test_matrix, metric):
+    if metric == "manhattan":
+        matrix_delta = manhattan_metric(train_matrix, test_matrix)
+    if metric == "euclidean":
+        matrix_delta = euclidean_metric(train_matrix, test_matrix)
+    if metric == "camberra":
+        matrix_delta = camberra_metric(train_matrix, test_matrix)
+    return matrix_delta
+
+
+def manhattan_metric(train_matrix, test_matrix):
+    matrix_delta = np.zeros(test_matrix.shape[0], train_matrix.shape[0])
+    for q in test_matrix:
+        for idx, instance in enumerate(train_matrix):
+            abs_differences = np.abs(q - instance)
+            matrix_delta[q][idx] += np.sum(abs_differences)
+    return matrix_delta
+
+
+def euclidean_metric(train_matrix, test_matrix):
+    matrix_delta = np.zeros(test_matrix.shape[0], train_matrix.shape[0])
+    for q in test_matrix:
+        for idx, instance in enumerate(train_matrix):
+            temp_sum = np.sum(np.square(q - instance))
+            matrix_delta[idx] += np.sqrt(temp_sum)
+    return matrix_delta
+
+
+def camberra_metric(train_matrix, test_matrix):
+    matrix_delta = np.zeros(test_matrix.shape[0], train_matrix.shape[0])
+    for q in test_matrix:
+        for idx, instance in enumerate(train_matrix):
+            numerator = np.abs(q - instance)
+            denominator = np.abs(q + instance)
+            matrix_delta[idx] += np.sum(numerator / denominator)
+    return matrix_delta
