e52666e2fbe0dcc3e062d0780d07ad9f5867eff7
Estela23 <estela.monxardina@gmail.com>
2020-11-28 17:32:31 +0100

    added inverse distance weighted policy

--- policies_knn.py
+++ policies_knn.py
@@ -16,7 +16,7 @@ def majority_class(matrix_k_neighbours, list_y_train):
     for n_instance, neighbours in enumerate(matrix_k_neighbours):
         labels_neighbours = list_y_train[n_instance]
         labels_dict = {}
-        for label in labels_neighbours:
+        for label in set(labels_neighbours):
             count_label = labels_neighbours.count(label)
             labels_dict[label] = count_label
         possible_labels = [k for k, v in labels_dict.items() if v == labels_dict[max(labels_dict, key=labels_dict.get)]]
@@ -29,6 +29,29 @@ def majority_class(matrix_k_neighbours, list_y_train):
                     predicted_labels.append(predicted)
                     break
 
+    return predicted_labels
+
+
+def inverse_distance_weighted(matrix_k_neighbours, list_y_train):
+    matrix_k_neighbours = np.array(matrix_k_neighbours)
+    predicted_labels = []
+    for n_instance, neighbours in enumerate(matrix_k_neighbours):
+        labels_neighbours = list_y_train[n_instance]
+        weights_neighbours = 1 / np.array(neighbours)
+        labels_dict = {}
+        for label in set(labels_neighbours):
+            indexes = [i for i in range(len(labels_neighbours)) if labels_neighbours[i] == label]
+            weight_label = np.sum(weights_neighbours[indexes])
+            labels_dict[label] = weight_label
+        possible_labels = [k for k, v in labels_dict.items() if v == labels_dict[max(labels_dict, key=labels_dict.get)]]
+        if len(possible_labels) == 1:
+            predicted_labels.append(possible_labels[0])
+        else:
+            for label in labels_neighbours:
+                if label in possible_labels:
+                    predicted = label
+                    predicted_labels.append(predicted)
+                    break
     return predicted_labels
 
 
