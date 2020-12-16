c27e6506b31a391c40d2500592c22d401ae5492f
Tair <tair31@gmail.com>
2020-12-16 23:58:33 +0100

    load hypo data with classes in the matrix

--- data_cleaning/load_hypo.py
+++ data_cleaning/load_hypo.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from data_cleaning import exploring, hypothyroid_cleaning
 from scipy.io.arff import loadarff
+import numpy as np
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -35,23 +36,22 @@ def load_train_test_fold(dataset_path: str, num_fold: int):
     test_data = pd.DataFrame(temp_data2[0])
     n_train = len(train_data)
     whole_data = pd.concat([train_data, test_data])
-    classes, df_data, numeric_columns, num_drop_upper = hypothyroid_cleaning.clean_filling_sex_and_filling_nans(whole_data, n_train)
+    df_data, numeric_columns, num_drop_upper = hypothyroid_cleaning.clean_filling_sex_and_filling_nans(whole_data, n_train)
     # sending n_train to understand if we drop any samples from train data or from test data
     min_df, max_df = numerical_min_max_calc(df_data, numeric_columns)
-    classes_train = classes.iloc[0:n_train-num_drop_upper]
+    # classes_train = classes.iloc[0:n_train-num_drop_upper]
     X_train = df_data.iloc[0:n_train-num_drop_upper, :]
     X_train = normalize(X_train, min_df, max_df, numeric_columns)
-    classes_test = classes.iloc[n_train-num_drop_upper:len(df_data)]
+    # classes_test = classes.iloc[n_train-num_drop_upper:len(df_data)]
     X_test = df_data.iloc[n_train-num_drop_upper:len(df_data), :]
     X_test = normalize(X_test, min_df, max_df, numeric_columns)
 
-    return classes_train, X_train, classes_test, X_test
+    return X_train.to_numpy(), X_test.to_numpy()
 
 
-# example - load fold 5
-classes_train, X_train, classes_test, X_test = load_train_test_fold('datasets/hypothyroid', 1)
-print(len(X_train))
-print(len(X_test))
-print(len(classes_train))
-print(len(classes_test))
+# example - load fold
+# X_train, X_test = load_train_test_fold('datasets/hypothyroid', 2)
+# print(np.shape(X_train))
+# print(np.shape(X_test))
+# print(X_train[0:21,-1])
 
