41f7fd7aa4744a5bbc9c5c371737f2d2fec8c5ad
Tair <tair31@gmail.com>
2020-12-17 20:42:16 +0100

    change names of funtions from string

--- evaluation.py
+++ evaluation.py
@@ -2,10 +2,13 @@ from data_cleaning import load_kropt, load_hypo
 from KNN import knn
 import numpy as np
 import time
-
+from distance_metric import manhattan_metric, euclidean_metric, camberra_metric
+from policies_knn import majority_class, inverse_distance_weighted, sheppards_work
+from weighting_knn import equal_weight, info_gain, reliefF
 
 # Run the desired algorithm and count the number of cases correctly classified,
 # the number of cases incorrectly classified, and the problem-solving time.
+
 
 def apply_model(distance_function, k, voting_function, weighting_function, train_data, test_data):
     start_time = time.time()
@@ -46,10 +49,10 @@ def apply_model_on10folds(data_name, distance_function, k, voting_function, weig
 
 def run_experiment(data_name):
     # define the knn we choose
-    distance_function = ["manhattan", "euclidean", "camberra"]
+    distance_function = [manhattan_metric, euclidean_metric, camberra_metric]
     k = [1, 3, 5, 7]
-    voting_function = ["majority_class", "inverse_distance_weighted", "sheppards_work"]
-    weighting_function = ["equal_weight", "info_gain", "reliefF"]
+    voting_function = [majority_class, inverse_distance_weighted, sheppards_work]
+    weighting_function = [equal_weight, info_gain, reliefF]
     file = open("results_knn.txt", "w")
     for ind_dist in range(len(distance_function)):
         for ind_k in range(len(k)):
@@ -59,8 +62,8 @@ def run_experiment(data_name):
                         apply_model_on10folds(data_name, distance_function[ind_dist], k[ind_k],
                                               voting_function[ind_vote], weighting_function[ind_weight])
                 # write results to txt
-                file.write('[' + distance_function[ind_dist] + ', ' + str(k[ind_k]) + ', ' + voting_function[ind_vote] +
-                           ', ' + weighting_function[ind_weight] + ' ]')
+                file.write('[' + distance_function[ind_dist].__name__ + ', ' + str(k[ind_k]) + ', ' +
+                           voting_function[ind_vote].__name__ + ', ' + weighting_function[ind_weight].__name__ + ' ]')
                 file.write("\tAccuracy:   " + str(accuracy) + "; correctly classified: " + str(correctly_classified) +
                            "; incorrectly classified: " + str(incorrectly_classified) + ";\tTime:   " + str(time_res) +
                            " \n")
