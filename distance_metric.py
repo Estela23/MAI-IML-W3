903cf9a1185c7da5a87e42fae93aa16368404eba
Alberto <alberto.soutullo@hotmail.com>
2020-12-17 04:13:45 +0100

    speed up manhattan function

--- distance_metric.py
+++ distance_metric.py
@@ -11,14 +11,26 @@ def distances_metrics(train_matrix, test_matrix, metric, feature_weights):
         matrix_distances = camberra_metric(train_matrix, test_matrix, feature_weights)
     return matrix_distances
 
-
+"""
 def manhattan_metric(train_matrix, test_matrix, feature_weights):
     matrix_distances = np.zeros((test_matrix.shape[0], train_matrix.shape[0]))
     for idy, q in enumerate(test_matrix):
         for idx, instance in enumerate(train_matrix):
             abs_differences = np.dot(feature_weights, np.abs(q - instance))
             matrix_distances[idy][idx] += abs_differences  # np.sum(abs_differences)
+
     return matrix_distances
+"""
+
+
+def manhattan_metric(train_matrix, test_matrix, feature_weights):
+
+    #matrix_distances = np.abs(train_matrix[:, 0, None] - test_matrix[:, 0]) + \
+    #                   np.abs(train_matrix[:, 1, None] - test_matrix[:, 1])
+
+    #matrix_distances = np.dot(feature_weights, np.abs(train_matrix[:, None] - test_matrix).sum(-1))
+    matrix_distances = np.sum(feature_weights * (np.abs(train_matrix[:, None, :] - test_matrix[None, :, :])), axis=-1)
+    return matrix_distances.T
 
 
 def euclidean_metric(train_matrix, test_matrix, feature_weights):
