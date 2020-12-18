4fca90e805bdf6f645a7111d6820d3abbcaadbac
Estela23 <estela.monxardina@gmail.com>
2020-12-18 19:10:31 +0100

    FCNN added, need to debugg

--- KNN/reduction_techniques/FCNN_rule.py
+++ KNN/reduction_techniques/FCNN_rule.py
@@ -1,4 +1,17 @@
 import numpy as np
+from scipy.spatial import distance
+from data_cleaning import load_hypo
+
+"""
+def calculate_distance(x1, x2):
+    return distance.cdist(np.array(x1), np.array(x2), metric="euclidean")
+"""
+
+
+def calculate_distance_vectors(x1, x2):
+    if x1 is None or x2 is None:
+        return float('inf')
+    return np.linalg.norm(np.array(x1)-np.array(x2))
 
 
 def compute_centroids(train_data):
@@ -11,11 +24,33 @@ def compute_centroids(train_data):
             temp_sum += instances_i[j]
         centroid_i = temp_sum / len(instances_i)
         centroids.append(centroid_i)
-    return np.array(centroids)
+    return centroids
 
 
 def FCNN_rule(train_data):
-    centroids = compute_centroids(train_data)
-    delta_S = centroids
+    T = train_data[:, :-1]
+    S = np.empty((0, train_data.shape[1]-1))
+    delta_S = compute_centroids(train_data)
+    while len(delta_S) != 0:
+        S = np.vstack((S, np.array(delta_S)))
+        rep_p = [None] * S.shape[0]
+        T_minus_S = [elem for elem in T if elem not in S]
+        nearest_q = [None] * len(T_minus_S)
+        for idx, q in enumerate(T_minus_S):
+            for p in delta_S:
+                if calculate_distance_vectors(nearest_q[idx], q) > calculate_distance_vectors(p, q):
+                    nearest_q[idx] = p
+            # TODO: arreglar estos índices del rep_p ...
+            if train_data[q, -1] != train_data[nearest_q[idx], -1] and \
+                    calculate_distance_vectors(nearest_q[idx], q) < calculate_distance_vectors(nearest_q[idx], rep_p[idx]):
+                rep_p[nearest_q[idx]] = q
+        delta_S = []
+        for rep in rep_p:
+            if rep is not None:
+                delta_S.append(rep)
+    return S
 
-    return reduced_train_data
+
+train_data, test_data = load_hypo.load_train_test_fold('datasets/hypothyroid', 1)
+
+reduced_train_data = FCNN_rule(train_data)
