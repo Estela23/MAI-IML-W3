ab255b63218e0f18dff32a360c14f8b12d7397cc
Alberto <alberto.soutullo@hotmail.com>
2020-12-19 22:19:44 +0100

    Fixed ib2

--- KNN/reduction_techniques/ib2.py
+++ KNN/reduction_techniques/ib2.py
@@ -6,7 +6,7 @@ def ib2(data_to_fit, knn, **_kwargs):
     subset = data_to_fit[:knn._k+1, :]
 
     for instance in data_to_fit:
-        knn.fit(subset)
+        knn._data = subset  # TODO: check this is correct
         class_results = knn.predict(instance[None, :])
         if class_results == instance[-1]:
             subset = np.vstack((subset, instance))
