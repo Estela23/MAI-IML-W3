bc9850b568aeff4b5a2f6ed9555265ab7503e30a
Tair <tair31@gmail.com>
2020-12-19 14:59:35 +0100

    enn update

--- KNN/reduction_techniques/ENN_rule.py
+++ KNN/reduction_techniques/ENN_rule.py
@@ -20,23 +20,22 @@ from data_cleaning import load_kropt, load_hypo
 #     return subset
 
 
-def enn(data_to_fit, predictions):
-#     classes = data_to_fit[:, -1]
+def ENN(data_to_fit, predictions):
+    subset = data_to_fit
+    idx_adjusted = 0
     for idx in range(len(predictions)):
-        if predictions[idx] != data_to_fit[:, -1]:
-            data = np.delete(data, 1, idx)
-#             classes = np.delete(classes, 0, idx)
+        if predictions[idx] == subset[idx_adjusted, -1]:
+            idx_adjusted = idx_adjusted + 1
+        else:
+            subset = np.delete(subset, (idx_adjusted), axis = 0)
 
-#     subset = np.concatenate((data, classes), axis=1)
     return subset
 
 
 train_data, test_data = load_hypo.load_train_test_fold('datasets/hypothyroid', 1)
+print(np.shape(train_data))
 model = KNN(euclidean_metric, 3, majority_class, equal_weight, verbose=False)
 model.fit(train_data)
 predictions = model.predict(train_data)
-time_init = time.time()
-subset = enn(train_data, predictions)
-time_end = time.time()
+subset = ENN(train_data, predictions)
 print(np.shape(subset))
-print(f"Time calculating ENN: {time_end - time_init}")
