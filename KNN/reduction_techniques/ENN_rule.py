adbefebfaa12a6ccc81652d74ee4270da0152616
Tair <tair31@gmail.com>
2020-12-19 20:37:36 +0100

    enn hopefully final

--- KNN/reduction_techniques/ENN_rule.py
+++ KNN/reduction_techniques/ENN_rule.py
@@ -1,23 +1,12 @@
 import numpy as np
 from KNN.knn import KNN
 import time
-from KNN.algorithms.distance_metrics import euclidean_metric
-from KNN.algorithms.policies_knn import majority_class
-from KNN.algorithms.weighting_knn import equal_weight
 from data_cleaning import load_hypo
+from evaluation import apply_model
+from KNN.algorithms.distance_metrics import manhattan_metric, euclidean_metric, camberra_metric
+from KNN.algorithms.policies_knn import majority_class, inverse_distance_weighted, sheppards_work
+from KNN.algorithms.weighting_knn import equal_weight, info_gain, reliefF
 
-
-# def enn(data_to_fit, knn):
-#     classes = data_to_fit[:, -1]
-#     data = data_to_fit[:, 0:-2]
-#     for idx, instance in enumerate(data):
-#         class_results = knn.predict(instance)
-#         if class_results != classes[idx]:
-#             data = np.delete(data, 1, idx)
-#             classes = np.delete(classes, 0, idx)
-#
-#     subset = np.concatenate((data, classes), axis=1)
-#     return subset
 
 
 def ENN(data_to_fit, predictions):
@@ -32,10 +21,21 @@ def ENN(data_to_fit, predictions):
     return subset
 
 
+"""
 train_data, test_data = load_hypo.load_train_test_fold('datasets/hypothyroid', 1)
 print(np.shape(train_data))
-model = KNN(euclidean_metric, 3, majority_class, equal_weight, verbose=False)
+model = KNN(euclidean_metric, 1, majority_class, equal_weight, verbose=False)
 model.fit(train_data)
 predictions = model.predict(train_data)
 subset = ENN(train_data, predictions)
 print(np.shape(subset))
+
+
+correct_class, time_folds = apply_model(euclidean_metric, 1, majority_class, equal_weight, subset, test_data)
+incorrect_class = len(test_data) - correct_class  # broadcasting: number of instances minus the correctly classified
+correctly_classified = np.average(correct_class)
+accuracy = correct_class / len(test_data)
+print(correct_class)
+print(accuracy)
+print(time_folds)
+"""
