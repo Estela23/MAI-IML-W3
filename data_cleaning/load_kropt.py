216de517f9f685d1efd5e027bcf634212cf4fb15
Alberto <alberto.soutullo@hotmail.com>
2020-12-20 15:09:17 +0100

    Fixed kropt parsing

--- data_cleaning/load_kropt.py
+++ data_cleaning/load_kropt.py
@@ -1,16 +1,20 @@
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from data_cleaning import exploring
 import os
 import numpy as np
 import pandas as pd
 
 
-def load_and_clean_kropt(file_full_path):
+def load_and_clean_kropt(file_full_path_train, file_full_path_test):
     """gets a full kropt path, loading and cleaning it"""
-    df_data = exploring.parse_arff_file_to_df(file_full_path)
-    df_data = decode_nominal_string_variables_by_column_list(df_data, df_data[:])
+    df_data_train = exploring.parse_arff_file_to_df(file_full_path_train)
+    df_data_test = exploring.parse_arff_file_to_df(file_full_path_test)
+    train_samples = len(df_data_train)
+    all_data = df_data_train.append(df_data_test, ignore_index=True)
+    all_data = decode_nominal_string_variables_by_column_list(all_data, all_data[:])
     # We store the class information for future references
     # classes = df_data["game"]
-    df_data["game"] = pd.factorize(df_data["game"])[0] + 1
+    # all_data["game"] = pd.factorize(all_data["game"])[0] + 1 todo here
     # print(df_data.head())
     # We get rid of class information.
     # df_data = df_data.drop("game", axis=1)
@@ -18,13 +22,24 @@ def load_and_clean_kropt(file_full_path):
     # label encoding the data
     # in case it is a column of chess - letter converted to int, else an integer is converted to int
     # division by 8 to normalize to 0-1
-    for col in df_data[:]:
-        if 'col' in col:
-            df_data[col] = [letter_to_labels(item) / 8 for item in df_data[col]]
-        elif 'row' in col:
-            df_data[col] = [int_to_label(item) / 8 for item in df_data[col]]
+    # for col in all_data[:]: todo here
+    #     if 'col' in col:
+    #         all_data[col] = [letter_to_labels(item) / 8 for item in all_data[col]]
+    #     elif 'row' in col:
+    #         all_data[col] = [int_to_label(item) / 8 for item in all_data[col]]
 
-    return df_data.to_numpy()
+    # label encoding the data
+    le = LabelEncoder()
+    for col in all_data[:]:
+        all_data[col] = le.fit_transform(all_data[col])
+
+    scaler = MinMaxScaler()
+    all_data.iloc[:, :-1] = pd.DataFrame(scaler.fit_transform(all_data.iloc[:, :-1]), columns=all_data.columns[:-1])
+
+    df_data_train = all_data.iloc[:train_samples, :]
+    df_data_test = all_data.iloc[train_samples:, :]
+
+    return df_data_train.to_numpy(), df_data_test.to_numpy()
 
 
 def letter_to_labels(letter):
@@ -44,8 +59,8 @@ def load_train_test_fold(dataset_path: str, num_fold: int):
     files_list = os.listdir(dataset_path)
     train_file = files_list[num_fold * 2 + 1]
     test_file = files_list[num_fold * 2]
-    train_data = load_and_clean_kropt(dataset_path.joinpath(train_file))
-    test_data = load_and_clean_kropt(dataset_path.joinpath(test_file))
+    train_data, test_data = load_and_clean_kropt(dataset_path.joinpath(train_file), dataset_path.joinpath(test_file))
+
     return train_data, test_data
 
 
